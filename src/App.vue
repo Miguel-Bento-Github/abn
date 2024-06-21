44fcafff3402facaf6f93fd47c2012c3be7a335d
b3nt0 <document.body.title@gmail.com>
2024-06-21 14:12:47 +0200

    remove routes from UI

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <script setup lang="ts">
-import LocaleChanger from "@/components/LocaleChanger.vue";
-import { Route } from "@/router/routes.enum";
+import LocaleChanger from "@/components/locale-changer/LocaleChanger.vue";
 </script>
 
 <template>
   <LocaleChanger />
-  <nav>
-    <RouterLink :to="Route.Home">{{ $t("home") }}</RouterLink>
-    |
-    <RouterLink :to="'404'">{{ $t("404") }}</RouterLink>
-  </nav>
   <RouterView />
 </template>
